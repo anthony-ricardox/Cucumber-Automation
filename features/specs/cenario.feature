b9defe7d0d3cd81b0317e1f1389dee2b60cdc9d6
anthony-ricardox <anthonyricardoact9@gmail.com>
2026-09-24 17:52:32 -0300

    usando cucumber para fazer soma de um cenario

--- features/specs/cenario.feature
+++ features/specs/cenario.feature
@@ -6,3 +6,9 @@ Cenario: Fazer Subtração
 Dado eu tenha 10 laranjas 
 Quando eu como 2 laranjas 
 Então eu vejo quantas laranjas sobraram.
+
+Cenario: Fazer Soma.
+
+Dado eu tenha 10 laranjas 
+Quando eu compro 5 laranjas 
+Então eu vejo quantas laranjas eu tenho.
--- features/step_definitions/cenario.step.rb
+++ features/step_definitions/cenario.step.rb
@@ -14,3 +14,15 @@ Então('eu vejo quantas laranjas sobraram.') do
   expect(@results ).to eq 8
   
 end
+
+
+Quando('eu compro {int} laranjas') do |c|
+# Quando('eu compro {float} laranjas') do |float|
+ @comprar = c
+  @total =   @quantidade + @comprar
+end
+
+Então('eu vejo quantas laranjas eu tenho.') do
+  puts @total
+  expect(@total ).to eq 15
+end
